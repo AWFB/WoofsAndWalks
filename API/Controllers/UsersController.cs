392678aa2e6271884d03d272c28435db738acff3
Antony Beardsmore <sider@me.com>
2022-11-27 21:51:04 +0000

    Added basic JWT Auth for testing

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,13 +1,13 @@
 ﻿using API.Data;
 using API.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-[ApiController]
-[Route("api/[controller]")] // /api/users
-public class UsersController : ControllerBase
+[Authorize]
+public class UsersController : BaseApiController
 {
     private readonly DataContext _context; // allows access outside of constructor
 
@@ -17,6 +17,7 @@ public class UsersController : ControllerBase
     }
     
     // Get all users
+    [AllowAnonymous]
     [HttpGet]
     public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
     {
